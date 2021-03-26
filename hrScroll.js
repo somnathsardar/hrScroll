94ed895596ec5d3169128f903d080032b8a56dc9
Somnath Sardar <githubsomnath1996@gmail.com>
2021-03-26 20:41:49 +0530

    JS updated.

--- hrScroll.js
+++ hrScroll.js
@@ -1,44 +1,51 @@
-let hrsConfig = {
-  container: '.hrs-container',
-  blocks: '.hrs-block',
-  offset: 0,
-  blockCount: 1,
-}
-
-$(window).scroll(e=>{
-  _heckHRSPOsition();
-});
-
-function initHRS(options){
-  if(options.container)
-    hrsConfig.container = options.container;
-  if(options.blocks)
-    hrsConfig.blocks = options.blocks;
-  if(options.offset)
-    hrsConfig.offset = options.offset;
-  hrsConfig.blockCount = $(hrsConfig.container +' '+ hrsConfig.blocks).length - 1;
-  
-  _heckHRSPOsition();
-}
-
-function _heckHRSPOsition(){
-  const result = _isInViewport(document.querySelector(hrsConfig.container));
-  const isElementInsideViewport = result.isVisible;
-  if(isElementInsideViewport){
-    const difference = ((window.innerHeight - hrsConfig.offset ) - result.position.top) / (window.innerHeight - hrsConfig.offset);
-    let scrollPercentRounded = Math.round(difference*100) * hrsConfig.blockCount;
-    $(hrsConfig.container).css({transform:`translateX(-${scrollPercentRounded}%)`});
+class hrsScroll{
+  constructor(options){
+    this.container = options.container != undefined ? options.container : '.hrs-container';
+    this.blocks = options.blocks != undefined ? options.blocks : '.hrs-block';
+    this.offset = options.offset != undefined ? options.offset : 0;
+    this.blockCount = $(this.container +' '+ this.blocks).length;
+    const sections= $(".hrs-container-1 .hrs-block");
+    let totalHeight = 0;
+    $.each(sections,(index, value)=>{
+      totalHeight += value.clientHeight;
+    });
+    $(this.container).css({'height':totalHeight});
   }
-}
 
-function _isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  const data = {
-                  isVisible:  rect.top >= 0 && rect.top + hrsConfig.offset <= window.innerHeight,
-                  position: {top: rect.top, right: rect.right, bottom: rect.bottom, left: rect.left},
-                  height: $(element).height(),
-                  width: $(element).width(),
-                  offsetTop: $(element).offset().top
-                }
-  return (data);
+  getConfig(){
+    return {
+              container: this.container,
+              blocks: this.blocks,
+              offset: this.offset,
+              blockCount: this.blockCount,
+            };
+  }
+
+  initHrs(){
+    const result = this._isInViewport(document.querySelector(this.container));
+    const isElementInsideViewport = result.isVisible;
+    if(isElementInsideViewport){
+      $(this.container+' span').css({'position':'sticky', 'top':'0', 'right': '0', 'left':'0', 'bottom': '0'});
+      let scrollAcmountFromTop = $(window).scrollTop();
+      let containerTopOffset = $(this.container)[0].offsetTop;
+      let diff = (( scrollAcmountFromTop - containerTopOffset) / result.height ) * 100 * this.blockCount;
+      $(this.container+' span').css({transform:`translateX(-${diff}%)`});
+    }
+    else{
+      $(this.container+' span').css({'position':'relative'});
+      $(this.container+' span').css({transform:`translateX(0%)`});
+    }
+  }
+
+  _isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    const data = {
+                    isVisible:  rect.top + this.offset < 0 && rect.bottom + this.offset > 0,
+                    position: {top: rect.top, right: rect.right, bottom: rect.bottom, left: rect.left},
+                    height: $(element).height(),
+                    width: $(element).width(),
+                    offsetTop: $(element).offset().top
+                  }
+    return (data);
+  }
 }
